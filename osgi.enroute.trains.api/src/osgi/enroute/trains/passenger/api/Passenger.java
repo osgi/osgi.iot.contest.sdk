6097db34eed55b5fa930ed016141db0a7fe2138e
Tim Verbelen <tim.verbelen@intec.ugent.be>
2016-07-07 15:15:31 +0200

    add convenience constructor for Passenger from Person

--- osgi.enroute.trains.api/src/osgi/enroute/trains/passenger/api/Passenger.java
+++ osgi.enroute.trains.api/src/osgi/enroute/trains/passenger/api/Passenger.java
@@ -8,6 +8,17 @@ package osgi.enroute.trains.passenger.api;
  */
 public class Passenger extends Person {
 
+	public Passenger(){}
+	
+	public Passenger(Person p){
+		this.id = p.id;
+		this.firstName = p.firstName;
+		this.lastName = p.lastName;
+		this.email = p.email;
+		this.company = p.company;
+		this.phone = p.phone;
+	}
+	
 	// on which train the passenger is currently on?
 	public String onTrain = null;
 	// in which station the passenger is currently waiting?
