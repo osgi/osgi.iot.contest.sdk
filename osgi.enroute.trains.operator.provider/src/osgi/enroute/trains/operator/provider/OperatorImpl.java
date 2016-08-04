00a38d2229c56758ebcbaacecc4c023c5e277535
Tim Verbelen <tim.verbelen@intec.ugent.be>
2016-08-04 20:16:46 +0200

    publish notifications when train is experiencing delay

--- osgi.enroute.trains.operator.provider/src/osgi/enroute/trains/operator/provider/OperatorImpl.java
+++ osgi.enroute.trains.operator.provider/src/osgi/enroute/trains/operator/provider/OperatorImpl.java
@@ -1,5 +1,6 @@
 package osgi.enroute.trains.operator.provider;
 
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
@@ -16,6 +17,7 @@ import org.osgi.service.metatype.annotations.Designate;
 import org.osgi.service.metatype.annotations.ObjectClassDefinition;
 
 import osgi.enroute.dto.api.DTOs;
+import osgi.enroute.scheduler.api.CancellablePromise;
 import osgi.enroute.scheduler.api.Scheduler;
 import osgi.enroute.trains.cloud.api.Observation;
 import osgi.enroute.trains.cloud.api.Observation.Type;
@@ -26,6 +28,7 @@ import osgi.enroute.trains.operator.api.ScheduleEntry;
 import osgi.enroute.trains.operator.api.TrainOperator;
 import osgi.enroute.trains.operator.provider.OperatorImpl.Config;
 import osgi.enroute.trains.passenger.api.Passenger;
+import osgi.enroute.trains.stations.api.StationObservation;
 import osgi.enroute.trains.stations.api.StationsManager;
 
 /**
@@ -41,7 +44,7 @@ import osgi.enroute.trains.stations.api.StationsManager;
 public class OperatorImpl implements TrainOperator, EventHandler {
 
 	private static int BOARD_TIME = 30000;
-	private static int TRAVEL_TIME = 120000;
+	private static int TRAVEL_TIME = 80000;
 	
     @ObjectClassDefinition
     @interface Config {
@@ -72,6 +75,8 @@ public class OperatorImpl implements TrainOperator, EventHandler {
 	private String name;
 	private List<String> trains = new ArrayList<>();
 	private List<String> stations = new ArrayList<>();
+
+	private Map<String, CancellablePromise<Instant>> timeouts = new HashMap<>();
 	
 	// keep the passengers currently on the trains
 	private Map<String, List<Passenger>> passengersOnTrains = new HashMap<>();
@@ -107,7 +112,7 @@ public class OperatorImpl implements TrainOperator, EventHandler {
 			e.start = stations.get(k);
 			e.departureTime = time;
 			e.destination = k+1 < stations.size() ? stations.get(k+1) : stations.get(0);
-			e.arrivalTime = time + TRAVEL_TIME; 
+			e.arrivalTime = e.departureTime + TRAVEL_TIME; 
 				
 			schedule.entries.add(e);
 			schedules.put(train, schedule);
@@ -115,10 +120,17 @@ public class OperatorImpl implements TrainOperator, EventHandler {
 			// schedule train start
 			scheduler.at(time).then(
 					p -> {
-						String dest = schedules.get(train).entries.get(0).start;
+						ScheduleEntry s = schedules.get(train).entries.get(0);
+						String dest = s.start;
 						String segment = stationsMgr.getStationSegment(dest);
 						System.out.println("Assign train "+train+" to station "+dest+" (segment "+segment+")");
 						tc.assign(train, segment);
+						
+						timeouts.put(train, scheduler.at(s.arrivalTime));
+						timeouts.get(train).then(pp -> {
+							delayed(train, s.destination);
+							return null;
+						});
 						return null;
 					},
 					p -> p.getFailure().printStackTrace()
@@ -132,9 +144,16 @@ public class OperatorImpl implements TrainOperator, EventHandler {
 			Observation o = EventToObservation.eventToObservation(event, dtos);
 			
 			if(o.type == Type.ASSIGNMENT_REACHED){
-				// notify arrival
 				String train = o.train;
 				String station = stationsMgr.getStation(o.assignment);
+
+				// cancel timeout
+				CancellablePromise<Instant> cp = timeouts.remove(train);
+				if(cp != null){
+					cp.cancel();
+				}
+				
+				// notify arrival
 				System.out.println("Train "+train+" arrived at "+station);
 				stationsMgr.arrive(train, station);
 
@@ -148,6 +167,12 @@ public class OperatorImpl implements TrainOperator, EventHandler {
 					passengersOnTrains.put(train, onBoard);
 					String segment = stationsMgr.getStationSegment(s.destination);
 					
+					timeouts.put(train, scheduler.at(s.arrivalTime));
+					timeouts.get(train).then(pp -> {
+						delayed(train, s.destination);
+						return null;
+					});
+					
 					System.out.println("Train "+train+" now has "+onBoard.size()+" passengers on board, leaving for "+s.destination);
 					tc.assign(train, segment);
 					
@@ -156,10 +181,10 @@ public class OperatorImpl implements TrainOperator, EventHandler {
 					ScheduleEntry extra = new ScheduleEntry();
 
 					extra.start = last.destination;
-					extra.departureTime = s.arrivalTime + BOARD_TIME;
+					extra.departureTime = Math.max(System.currentTimeMillis()+TRAVEL_TIME, s.arrivalTime) + BOARD_TIME;
 					int index = stations.indexOf(last.destination);
 					extra.destination = index+1 < stations.size() ? stations.get(index+1) : stations.get(0);
-					extra.arrivalTime = s.arrivalTime + TRAVEL_TIME;
+					extra.arrivalTime = extra.departureTime + TRAVEL_TIME;
 					schedule.entries.add(extra);
 					
 					// Remove first entry
@@ -196,4 +221,17 @@ public class OperatorImpl implements TrainOperator, EventHandler {
 		return new ArrayList<>(schedules.values());
 	}
 
+	private void delayed(String train, String destination){
+		StationObservation delay = new StationObservation();
+		delay.type = StationObservation.Type.NOTIFICATION;
+		delay.station = destination;
+		delay.train = train;
+		delay.message = "Train "+train+" traveling to "+destination+" is delayed";
+		try {
+			Event event = new Event(StationObservation.TOPIC, dtos.asMap(delay));
+			ea.postEvent(event);
+		} catch(Exception e){
+			System.err.println("Error sending notification Event: "+e.getMessage());
+		}
+	}
 }
