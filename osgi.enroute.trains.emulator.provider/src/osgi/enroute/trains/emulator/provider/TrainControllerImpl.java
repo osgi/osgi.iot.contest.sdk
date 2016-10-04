3aac1e095a25efce220ea717b48b5e972c47dd49
Velitchko Valkov <v.valkov@prosyst.bg>
2016-10-04 10:35:07 +0300

    Changes for Prosyst mBS Use Cases - added eventhandlers to respond to block/train lights events.

--- osgi.enroute.trains.emulator.provider/src/osgi/enroute/trains/emulator/provider/TrainControllerImpl.java
+++ osgi.enroute.trains.emulator.provider/src/osgi/enroute/trains/emulator/provider/TrainControllerImpl.java
@@ -6,6 +6,9 @@ import java.util.Random;
 
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.ServiceRegistration;
+import org.osgi.service.event.Event;
+import org.osgi.service.event.EventConstants;
+import org.osgi.service.event.EventHandler;
 import org.osgi.util.promise.Deferred;
 import org.osgi.util.promise.Promise;
 
@@ -15,7 +18,7 @@ import osgi.enroute.trains.controller.api.TrainLocator;
 import osgi.enroute.trains.track.util.Tracks.SegmentHandler;
 import osgi.enroute.trains.train.api.TrainController;
 
-public class TrainControllerImpl implements TrainController, TrainLocator {
+public class TrainControllerImpl implements TrainController, TrainLocator, EventHandler {
 
 	private double distance;
 	private int desiredSpeed;
@@ -27,6 +30,7 @@ public class TrainControllerImpl implements TrainController, TrainLocator {
 	private EmulatorImpl emulatorImpl;
 	private String name;
 	private final Random random = new Random();
+	private ServiceRegistration<EventHandler>    regEvent;
 
 	private double rfidProb;
     private Deferred<String> nextLocation = new Deferred<String>();
@@ -105,6 +109,7 @@ public class TrainControllerImpl implements TrainController, TrainLocator {
 
 	public void close() {
 		registration.unregister();
+		regEvent.unregister();
 	}
 
 	public void register(BundleContext context, String channel) {
@@ -113,7 +118,13 @@ public class TrainControllerImpl implements TrainController, TrainLocator {
 		properties.put("train.name", name);
 		properties.put("channel", channel);
 		registration = context.registerService(new String[]{TrainController.class.getName(), TrainLocator.class.getName()}, this, properties);
-	}
+    String[] topics = new String[] {
+        "osgi/trains/trainlight"
+    };
+    Hashtable ht = new Hashtable();
+    ht.put(EventConstants.EVENT_TOPIC, topics);
+    regEvent = context.registerService(EventHandler.class, this, ht);
+  }
 
     @Override
     public synchronized Promise<String> nextLocation() {
@@ -126,4 +137,16 @@ public class TrainControllerImpl implements TrainController, TrainLocator {
         currentLocation.resolve(trainId + ":" + segment);
     }
 
+  public void handleEvent(Event event) {
+    if (event.getProperty("trainname").equals(name)) {
+      if (event.getProperty("lights").equals("true")) {
+        light(true);
+        System.out.println("Turning on light! Train=" + event.getProperty("trainname"));
+      } else {
+        light(false);
+        System.out.println("Turning off light! Train=" + event.getProperty("trainname"));
+      }
+    }
+  }
+    
 }
