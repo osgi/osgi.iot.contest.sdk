d5d27e97f25647a7510c13f47486625024002b5c
Tim Verbelen <tim.verbelen@intec.ugent.be>
2017-10-03 11:10:45 +0200

    keep Station state in demo bundle

--- osgi.enroute.trains.demo/src/osgi/enroute/trains/demo/Station.java
+++ osgi.enroute.trains.demo/src/osgi/enroute/trains/demo/Station.java
@@ -2,7 +2,15 @@ package osgi.enroute.trains.demo;
 
 public class Station {
 
+	public enum Type {
+		CARGO, REGULAR
+	}
+	
 	public String name;
+
 	public String segment;
 
+	public Type type;
+	
+	public String train;
 }
--- osgi.enroute.trains.demo/src/osgi/enroute/trains/demo/TrainsDemo.java
+++ osgi.enroute.trains.demo/src/osgi/enroute/trains/demo/TrainsDemo.java
@@ -20,7 +20,8 @@ import org.osgi.util.converter.Converter;
 
 import osgi.enroute.debug.api.Debug;
 import osgi.enroute.mqtt.api.MQTTService;
-import osgi.enroute.scheduler.api.Scheduler;
+import osgi.enroute.trains.robot.api.RobotCommand;
+import osgi.enroute.trains.robot.api.RobotObservation;
 import osgi.enroute.trains.train.api.Assignment;
 import osgi.enroute.trains.train.api.Assignment.Type;
 
@@ -33,14 +34,13 @@ import osgi.enroute.trains.train.api.Assignment.Type;
 	property = {
 			Debug.COMMAND_SCOPE +"=trains",
 			Debug.COMMAND_FUNCTION + "=station",
-			Debug.COMMAND_FUNCTION + "=container",
-			Debug.COMMAND_FUNCTION + "=service"
-	}, service=TrainsDemo.class)
+			Debug.COMMAND_FUNCTION + "=container"},
+	service=TrainsDemo.class)
 public class TrainsDemo {
 
 	private Random random = new Random();
 	
-	private List<String> trains = new ArrayList<>();
+	private Map<String, Boolean> trains = new HashMap<>();
 	
 	// separate list of station names to facilitate random station selection
 	private List<String> stationList = new ArrayList<>();
@@ -74,40 +74,95 @@ public class TrainsDemo {
 		for(String s : config.stations()){
 			String[] split = s.split(":");
 			Station station = new Station();
-			station.name = split[0];
-			station.segment = split[1];
+			station.name = split[0].trim();
+			station.segment = split[1].trim();
+			station.type = Station.Type.valueOf(split[2].trim());
 			
 			stationList.add(station.name);
 			stations.put(station.name, station);
 		}
 		
 		for(String s : config.trains()){
-			trains.add(s);
-			scheduleRandomStation(s);
+			String[] split = s.split(":");
+			String train = split[0].trim();
+			boolean loaded = Boolean.parseBoolean(split[1].trim());
+			trains.put(train, loaded);
+			scheduleRandomStation(train);
 		}
 		
 		mqtt.subscribe(Assignment.TOPIC)
 			.map(msg -> converter.convert(msg.payload().array()).to(Assignment.class))
-			.filter(a -> a.type == Assignment.Type.REACHED)
 			.forEach(a -> {
-				// train reached assignment 
-				String train = a.train;
+				switch(a.type){
+				case REACHED:
+				{
+					// train reached assignment 
+					String train = a.train;
+					Optional<Station> station = stations.values().stream()
+						.filter(s -> s.segment.equals(a.segment))
+						.findFirst();
+					
+					if(station.isPresent()){
+						System.out.println("Train "+a.train+" reached station "+station.get().name);
+						
+						station.get().train = train;
+						
+						event("Train "+a.train+" reached station "+station.get().name);
+						
+						if(station.get().type == Station.Type.CARGO){
+							if(trains.get(a.train)){
+								// this train is loaded?
+								event("Unloading container from "+a.train);
+								container(false);
+							} else {
+								event("Loading container onto "+a.train);
+								container(true);
+							}
+						} else {
+							scheduleRandomStation(train);
+						}
+					} else {
+						// reschedule when assigned to custom segment?
+						scheduleRandomStation(train);
+					}
+					
+					
+					break;
+				}
+				case ASSIGN:
+				{	
+					Optional<Station> station = stations.values().stream()
+						.filter(s -> a.train.equals(s.train))
+						.findFirst();	
+					
+					if(station.isPresent()){
+						event("Train "+a.train+" leaving station "+station.get().name);
+						station.get().train = null;
+					}
+					break;
+				}
+			}
+		});
+		
+		
+		mqtt.subscribe(RobotObservation.TOPIC)
+			.map(msg -> converter.convert(msg.payload().array()).to(RobotObservation.class))
+			.forEach(a -> {
+
+				// check which train is in cargo station
 				Optional<Station> station = stations.values().stream()
-					.filter(s -> s.segment.equals(a.segment))
-					.findFirst();
+						.filter(s -> s.type == Station.Type.CARGO)
+						.findFirst();
 				
 				if(station.isPresent()){
-					System.out.println("Train "+a.train+" reached station "+station.get().name);
+					String train = station.get().train;
 					
-					// TODO send out station reached event
-					
+					if(train != null){
+						// reschedule
+						scheduleRandomStation(train);	
+					}
 				}
-				
-				// reschedule?
-				scheduleRandomStation(train);
-				
-			});
-		
+		});
 		
 		
 	}
@@ -128,14 +183,16 @@ public class TrainsDemo {
 	}
 	
 	
-	// send a train to the servicing station
-	public void service(String train){
-		
-	}
-	
 	// load/unload container
-	public void container(){
+	public void container(boolean load){
+		RobotCommand c = new RobotCommand();
+		c.type = load ? RobotCommand.Type.LOAD : RobotCommand.Type.UNLOAD;
 		
+		try {
+			mqtt.publish(RobotCommand.TOPIC,  ByteBuffer.wrap( converter.convert(c).to(byte[].class)));
+		} catch(Exception e){
+			e.printStackTrace();
+		}
 	}
 	
 	
@@ -146,6 +203,18 @@ public class TrainsDemo {
 		scheduler.schedule(()-> station(train, station), seconds, TimeUnit.SECONDS);
 	}
 	
+	
+	public void event(String message){
+		DemoEvent event = new DemoEvent();
+		event.time = System.currentTimeMillis();
+		event.message = message;
+		
+		try {
+			mqtt.publish(DemoEvent.TOPIC,  ByteBuffer.wrap( converter.convert(event).to(byte[].class)));
+		} catch(Exception e){
+			e.printStackTrace();
+		}
+	}
 	
 	//
 	
